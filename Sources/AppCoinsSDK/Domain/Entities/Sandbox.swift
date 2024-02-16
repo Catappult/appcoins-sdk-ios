ecc4afc6c5f21e42bcf62faee93e6aa4800c6cba
tiaguu <tiagogteodoro@gmail.com>
2024-02-16 17:31:45 +0000

    Adds a method for a developer to get the Wallet address in order to receive APPC and be able to test the integration.

---
+++ Sources/AppCoinsSDK/Domain/Entities/Sandbox.swift
@@ -0,0 +1,17 @@
+//
+//  Sandbox.swift
+//  
+//
+//  Created by aptoide on 16/02/2024.
+//
+
+import Foundation
+
+public struct Sandbox {
+    
+    // This method should be accessed by developers for testing purposes only
+    static public func getTestingWalletAddress() -> String? {
+        return WalletUseCases.shared.getClientWallet()?.address
+    }
+    
+}
