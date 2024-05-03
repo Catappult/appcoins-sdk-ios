21c7213519c50346b25b07538c8aed75b222ed07
tiaguu <tiagogteodoro@gmail.com>
2024-05-03 12:35:42 +0100

    Added marketplace verification to SDK isAvailable logic.

--- Sources/AppCoinsSDK/Domain/Entities/AppcSDK.swift
+++ Sources/AppCoinsSDK/Domain/Entities/AppcSDK.swift
@@ -7,12 +7,24 @@
 
 import Foundation
 import StoreKit
+import MarketplaceKit
 
 public struct AppcSDK {
     
     static public func isAvailable() async -> Bool {
-        if #available(iOS 17.4, *) { return true }
-        else { return false }
+        if #available(iOS 17.4, *) { 
+            do {
+                let storefront = try await AppDistributor.current
+                switch storefront {
+                case .marketplace(let marketplace):
+                    return marketplace == "com.aptoide.ios.store"
+                default:
+                    return false
+                }
+            } catch {
+                return false
+            }
+        } else { return false }
     }
 
     static public func handle(redirectURL: URL?) -> Bool {
