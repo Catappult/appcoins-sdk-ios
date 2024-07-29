486e6ccea8d8914cddefcd7852eba1f481135f1e
Graciano Caldeira <gracianocaldeira@GraciansMacBook.lan>
2024-07-29 11:56:35 +0100

    6 - Commented code deleted

--- Sources/AppCoinsSDK/Models/Raws/PayFlowDataRaw.swift
+++ Sources/AppCoinsSDK/Models/Raws/PayFlowDataRaw.swift
@@ -9,24 +9,6 @@ import Foundation
 
 internal struct PayFlowDataRaw: Codable {
     
-//    let paymentMethods: PaymentMethods
-//    
-//    enum CodingKeys: String, CodingKey {
-//        case paymentMethods = "payment_methods"
-//    }
-//}
-//
-//internal struct PaymentMethods: Codable {
-//    
-//    let iosSDK: IOSSdkData
-//    
-//    enum CodingKeys: String, CodingKey {
-//        case iosSDK = "ios_sdk"
-//    }
-//}
-//
-//internal struct IOSSdkData: Codable {
-    
     let paymentFlow: String
     let version: String
     let priority: Int
