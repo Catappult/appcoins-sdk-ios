2e56d709e7567fede28f02323f41926d6d1949ce
Graciano Caldeira <gracianocaldeira@GraciansMacBook.lan>
2024-07-30 11:06:31 +0100

    payFlowDataRaw.paymentFlow to lower case correction

--- Sources/AppCoinsSDK/Domain/Repositories/PayFlow/PayFlowRepository.swift
+++ Sources/AppCoinsSDK/Domain/Repositories/PayFlow/PayFlowRepository.swift
@@ -20,7 +20,7 @@ internal class PayFlowRepository: PayFlowRepositoryProtocol {
         self.PayFlowService.setPayFlow(package: BuildConfiguration.packageName, packageVercode: BuildConfiguration.packageVersion, sdkVercode: BuildConfiguration.vercode, locale: Locale.current.regionCode?.lowercased(), oemID: mmpRepository.getOEMID(), oemIDType: nil, country: nil, os: "ios") { result in
             switch result {
             case .success(let payFlowDataRaw):
-                try? Utils.writeToPreferences(key: "pay-flow-type", value: payFlowDataRaw.paymentFlow)
+                try? Utils.writeToPreferences(key: "pay-flow-type", value: payFlowDataRaw.paymentFlow.lowercased())
             case .failure:
                 break
             }
