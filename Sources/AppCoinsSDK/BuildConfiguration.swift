6b4c836ec189063154d89fa17027a3e26dda5c60
Graciano Caldeira <gracianocaldeira@GraciansMacBook.lan>
2024-08-30 17:19:56 +0100

    Release 1.3.0

--- Sources/AppCoinsSDK/BuildConfiguration.swift
+++ Sources/AppCoinsSDK/BuildConfiguration.swift
@@ -121,7 +121,7 @@ internal class BuildConfiguration {
     
     static internal var integratedMethods: [Method] = [.appc, .paypalAdyen, .paypalDirect, .creditCard, .sandbox]
     
-    static internal var sdkShortVersion: String = "1.2.0"
+    static internal var sdkShortVersion: String = "1.3.0"
 }
 
 internal enum SDKEnvironment: String {
