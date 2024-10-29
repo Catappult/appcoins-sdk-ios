16d9ba88288a3be83c39a994e85095c91ec56241
tiaguu <tiagogteodoro@gmail.com>
2024-10-29 16:36:16 +0000

    Fixed localization and images not loaded on swift package distribution.

--- Sources/AppCoinsSDK/Utils/Extensions.swift
+++ Sources/AppCoinsSDK/Utils/Extensions.swift
@@ -22,14 +22,17 @@ internal extension UIView {
 // For some reason this solves the issue 'module' is inaccessible due to 'internal' protection level
 internal extension Bundle {
     static internal let APPCModule: Bundle = {
+        #if SWIFT_PACKAGE
+        return Bundle.module
+        #else
+        // Fallback for non-Swift Package Manager (e.g., Framework)
         let bundleName = "AppCoinsSDK"
-
         let candidates = [
             Bundle.main.resourceURL,
             Bundle(for: BuildConfiguration.self).resourceURL,
             Bundle.main.bundleURL
         ]
-
+        
         for candidate in candidates {
             let bundlePath = candidate?.appendingPathComponent("\(bundleName).bundle")
             if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
@@ -37,5 +40,6 @@ internal extension Bundle {
             }
         }
         return Bundle(for: BuildConfiguration.self)
+        #endif
     }()
 }
