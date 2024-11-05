6d8b2430276634d3c8a0e9c5e6c07e74ef7d3fc4
Graciano Caldeira <gracianocaldeira@Gracianos-MBP.lan>
2024-11-05 10:25:47 +0000

    AppCoinGamificationServiceClient getTransactionBonus correction

--- Sources/AppCoinsSDK/Models/Services/Gamification/AppCoinGamificationServiceClient.swift
+++ Sources/AppCoinsSDK/Models/Services/Gamification/AppCoinGamificationServiceClient.swift
@@ -32,9 +32,16 @@ internal class AppCoinGamificationServiceClient : AppCoinGamificationService {
                         result(.failure(.failed(message: "Service Failed", description: "Failed to communicate with service on endpoint: \(url)")))
                     }
                 } else {
-                    if let data = data, let findResult = try? JSONDecoder().decode(TransactionBonusRaw.self, from: data) {
-                        result(.success(findResult))
-                    } else { result(.failure(.failed(message: "Service Failed", description: "Failed to communicate with service on endpoint: \(url) with response: \(String(data: data ?? Data(), encoding: .utf8))"))) }
+                    do {
+                        if let data = data {
+                            let successResponse = try JSONDecoder().decode(TransactionBonusRaw.self, from: data)
+                            result(.success(successResponse))
+                        } else {
+                            result(.failure(.failed(message: "Service Failed", description: "No data received from endpoint: \(url)")))
+                        }
+                    } catch {
+                        result(.failure(.failed(message: "Service Failed", description: "Failed to decode response from endpoint: \(url). Error: \(error.localizedDescription)", request: DebugRequestInfo(request: request, responseData: data, response: response))))
+                    }
                 }
             }
             task.resume()
