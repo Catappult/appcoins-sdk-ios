3c45c6772ad755e6f583cae05895fabeba4391b1
Graciano Caldeira <gracianocaldeira@Gracianos-MacBook-Pro.local>
2024-11-28 11:43:27 +0000

    PaymentMethodIcon size correction

--- Sources/AppCoinsSDK/UI/Purchase/PurchaseStateViews/SelectedPaymentMethodView.swift
+++ Sources/AppCoinsSDK/UI/Purchase/PurchaseStateViews/SelectedPaymentMethodView.swift
@@ -37,6 +37,7 @@ struct SelectedPaymentMethodView: View {
                                         HStack(spacing: 0) {
                                             PaymentMethodIcon(icon: paymentMethodSelected.icon, disabled: false)
                                                 .frame(maxWidth: .infinity, alignment: .leading)
+                                                .frame(width: 24, height: 24)
                                             
                                             VStack {}.frame(width: 16)
                                             
