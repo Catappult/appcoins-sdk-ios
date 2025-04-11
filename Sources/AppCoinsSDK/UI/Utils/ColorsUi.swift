9ff447b88ba23581e501552182216657bf041bff
Graciano Caldeira <gracianocaldeira@Gracianos-MBP.lan>
2025-04-11 12:43:30 +0100

    2 - ColorsUi replaced and refactored

---
+++ Sources/AppCoinsSDK/UI/Utils/ColorsUi.swift
@@ -0,0 +1,16 @@
+//
+//  ColorsUi.swift
+//  
+//
+//  Created by aptoide on 07/03/2023.
+//
+
+import Foundation
+import SwiftUI
+
+internal struct ColorsUi {
+    // Web Checkout
+    static internal var APC_WebViewDarkMode: Color = Color(red: 0.16, green: 0.16, blue: 0.2)
+    static internal var APC_WebViewLightMode: Color = Color(red: 0.96, green: 0.96, blue: 0.98)
+}
+
