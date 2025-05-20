60a602346e9903cc70123ff8c1f7570127224141
Graciano Caldeira <gracianocaldeira@Gracianos-MacBook-Pro.local>
2025-05-20 18:59:00 +0100

    Merge with epic/3549

--- Sources/AppCoinsSDK/Domain/UseCases/SDKUseCases.swift
+++ Sources/AppCoinsSDK/Domain/UseCases/SDKUseCases.swift
@@ -17,6 +17,12 @@ internal class SDKUseCases {
     internal func isDefault() -> Bool? { self.repository.isDefault() }
     internal func setSDKDefault(value: Bool) { self.repository.setSDKDefault(value: value) }
     
+    internal func getDefaultStorefrontLocale() -> String? { self.repository.getDefaultStorefrontLocale() }
+    internal func setSDKDefaultStorefrontLocale(locale: String) { self.repository.setSDKDefaultStorefrontLocale(locale: locale) }
+
+    internal func getDefaultStorefrontMarketplace() -> String? { self.repository.getDefaultStorefrontMarketplace() }
+    internal func setSDKDefaultStorefrontMarketplace(marketplace: String) { self.repository.setSDKDefaultStorefrontMarketplace(marketplace: marketplace) }
+    
     internal func persistPurchaseIntent(intent: PurchaseIntent) { repository.persistPurchaseIntent(intent: intent) }
     internal func fetchPurchaseIntent() -> PurchaseIntent? { return repository.fetchPurchaseIntent() }
     internal func removePurchaseIntent() { repository.removePurchaseIntent() }
--- Sources/AppCoinsSDK/Models/Local/UserPreferences/UserPreferencesLocalClient.swift
+++ Sources/AppCoinsSDK/Models/Local/UserPreferences/UserPreferencesLocalClient.swift
@@ -17,4 +17,22 @@ internal class UserPreferencesLocalClient: UserPreferencesLocalService {
     internal func setSDKDefault(value: String) {
         try? Utils.writeToPreferences(key: "is-sdk-default", value: value)
     }
+    
+    func getDefaultStorefrontLocale() -> String? {
+        let defaultStorefrontLocale: String = Utils.readFromPreferences(key: "sdk-default-storefront-locale")
+        return defaultStorefrontLocale == "" ? nil : defaultStorefrontLocale
+    }
+    
+    func setSDKDefaultStorefrontLocale(locale: String) {
+        try? Utils.writeToPreferences(key: "sdk-default-storefront-locale", value: locale)
+    }
+    
+    func getDefaultStorefrontMarketplace() -> String? {
+        let defaultStorefrontMarketplace: String = Utils.readFromPreferences(key: "sdk-default-storefront-marketplace")
+        return defaultStorefrontMarketplace == "" ? nil : defaultStorefrontMarketplace
+    }
+    
+    func setSDKDefaultStorefrontMarketplace(marketplace: String) {
+        try? Utils.writeToPreferences(key: "sdk-default-storefront-marketplace", value: marketplace)
+    }
 }
