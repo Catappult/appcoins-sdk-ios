4378c8b2ca1d4a28a62005664db3965222f5589d
Graciano Caldeira <gracianocaldeira@Gracianos-MacBook-Pro.local>
2025-05-22 11:57:09 +0100

    iOS version verification added. BottomSheet for new versions added

--- Sources/AppCoinsSDK/UI/Views/Purchase/PurchaseBottomSheet.swift
+++ Sources/AppCoinsSDK/UI/Views/Purchase/PurchaseBottomSheet.swift
@@ -17,8 +17,24 @@ internal struct PurchaseBottomSheet: View {
     
     internal var body: some View {
         if viewModel.isWebviewWebCheckoutPresented {
-            BottomSheetPresenter(isPresented: $viewModel.isWebviewWebCheckoutPresented)
-                .ignoresSafeArea(edges: .bottom)
+            if #available(iOS 17.4, *) {
+                HStack(spacing: 0) {}
+                    .sheet(isPresented: $viewModel.isWebviewWebCheckoutPresented, onDismiss: viewModel.dismiss, content: {
+                        VStack(spacing: 0) {
+                            VStack{}.frame(height: 20)
+                            
+                            WebCheckoutView()
+                                .frame(width: viewModel.orientation == .landscape ? UIScreen.main.bounds.width - 116: UIScreen.main.bounds.width)
+                                .presentationCompactAdaptation(.sheet)
+                                .presentationDetents([viewModel.orientation == .landscape ? .fraction(0.9) : .fraction(0.6)])
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .background(self.colorScheme == .dark ? ColorsUi.APC_WebViewDarkMode : ColorsUi.APC_WebViewLightMode)
+                    })
+            } else {
+                BottomSheetPresenter(isPresented: $viewModel.isWebviewWebCheckoutPresented)
+                    .ignoresSafeArea(edges: .bottom)
+            }
         }
 
         if viewModel.isBrowserWebCheckoutPresented {
