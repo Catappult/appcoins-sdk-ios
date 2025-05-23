1e18ce1a5295a01d84347645bf3923ddf971543f
Graciano Caldeira <gracianocaldeira@Gracianos-MacBook-Pro.local>
2025-05-23 12:07:53 +0100

    Uncomment previously commented-out code. Replaced PT with USA as it should be on the AppcSDK file

--- Sources/AppCoinsSDK/Domain/Entities/AppcSDK.swift
+++ Sources/AppCoinsSDK/Domain/Entities/AppcSDK.swift
@@ -126,7 +126,7 @@ public struct AppcSDK {
         
         do {
             let storefront = try await StoreKit.Storefront.current
-            return (storefront?.countryCode == "PT")
+            return (storefront?.countryCode == "USA")
         } catch {
             // If the Storefront lookup fails, fall back to the locale
             return localeIsUS
