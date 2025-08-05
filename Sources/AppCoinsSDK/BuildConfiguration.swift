24cdbf15b55da32a46b082116a971c04aceb98e2
tiaguu <tiagogteodoro@gmail.com>
2025-08-05 13:54:47 +0100

    Raised version number

--- Sources/AppCoinsSDK/BuildConfiguration.swift
+++ Sources/AppCoinsSDK/BuildConfiguration.swift
@@ -124,10 +124,10 @@ internal class BuildConfiguration {
     static internal var integratedMethods: [Method] = [.appc, .paypalAdyen, .paypalDirect, .creditCard, .sandbox]
     
     // WARNING: Changing this variable definition might break CI/CD
-    static internal var sdkShortVersion: String = "3.1.0"
+    static internal var sdkShortVersion: String = "3.2.0"
     
     // WARNING: Changing this variable definition might break CI/CD
-    static internal var sdkBuildNumber: Int = 41
+    static internal var sdkBuildNumber: Int = 42
 }
 
 internal enum SDKEnvironment: String {
