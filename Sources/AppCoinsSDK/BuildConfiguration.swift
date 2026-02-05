76cbbecdba60fc2135f178c3938bd452e729218d
tiaguu <tiagogteodoro@gmail.com>
2026-02-05 17:16:45 +0000

    Raised version number

--- Sources/AppCoinsSDK/BuildConfiguration.swift
+++ Sources/AppCoinsSDK/BuildConfiguration.swift
@@ -98,10 +98,10 @@ internal class BuildConfiguration {
     static internal var userUID =  UIDevice.current.identifierForVendor!.uuidString
     
     // WARNING: Changing this variable definition might break CI/CD
-    static internal var sdkShortVersion: String = "4.1.0"
+    static internal var sdkShortVersion: String = "4.1.1"
     
     // WARNING: Changing this variable definition might break CI/CD
-    static internal var sdkBuildNumber: Int = 51
+    static internal var sdkBuildNumber: Int = 52
 }
 
 internal enum SDKEnvironment: String {
